309bda0c804441b1dd03289c52cba37dabbe082d
Rita-R <rita.raad@outlook.com>
2015-05-31 20:25:52 -0300

    rent atualizada

--- assets/operations/rent.java
+++ assets/operations/rent.java
@@ -2,6 +2,8 @@ package assets.operations;
 
 
 import assets.books.*;
+
+
 import java.io.*;
 import java.util.*;
 
@@ -9,7 +11,10 @@ import java.util.*;
 classe rent, realiza o empréstimo
 */
 public class rent extends operations{
-    
+    String nameaux;
+    String titleaux;
+    int newlimit;
+    boolean returned;
     
     public void process (user novo, books book){
         this.nameaux = novo.get_UserName();
@@ -18,7 +23,49 @@ public class rent extends operations{
             newlimit = novo.get_UserLimit() - 1;
             novo.set_UserLimit(newlimit);
             book.set_Rent(true);
+            returned = false;
         }
     }
-
+    /*
+        Escreve registro do arquivo.
+        obs= adicionar campos data atual e data de devolução
+	*/
+        public void reg_writer()throws IOException{
+		BufferedWriter buffWrite = new BufferedWriter(new FileWriter("operations.csv",true));
+		buffWrite.append(this.nameaux + " "+this.titleaux+" "+this.returned+"\n");
+		buffWrite.close();
+	} 
+        
+        /*
+        Leitura de um objeto do arquivo
+        ALTERADA
+        */
+	public void read_rent(String csv)throws IOException{
+		String teste;
+		BufferedReader buffRead = new BufferedReader(new FileReader(csv));
+		teste=buffRead.readLine();
+		System.out.println(teste);
+		buffRead.close();
+	}
+        /* função que recupera todos os emprestimos */
+	public void recoverAllUsers(String csv)throws IOException{
+	String teste, parts[];
+	BufferedReader reading = new BufferedReader(new FileReader(csv));
+		
+		while(reading.ready()){
+			teste = reading.readLine();
+			parts=teste.split(" ");
+			this.nameaux=parts[0];
+			this.titleaux=parts[1];
+			
+			System.out.println("Usuário: "+nameaux);	
+			System.out.println("Título: "+this.titleaux);
+                        this.returned=Boolean.valueOf(parts[2]);
+                        if(this.returned)
+					System.out.print("devolvido");
+			else
+					System.out.print("pendente");
+		}
+		reading.close();
+	}
 } 
